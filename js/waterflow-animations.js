873c75ed4caf2bf34e22d164f7b65805f18315f1
Sylwia <sylviaskovron@gmail.com>
2018-10-01 03:32:10 +0200

    animations

--- js/waterflow-animations.js
+++ js/waterflow-animations.js
@@ -4,9 +4,16 @@ const user_input_weight = document.querySelector(".user-weight input[type='numbe
 const user_input_name = document.querySelector(".user-name input[type='text']");
 const user_input_back = document.querySelector(".inputs-group #back");
 const inputs = document.querySelector(".inputs-group");
+const user_name = document.querySelectorAll(".u_name");
 
-
-
+// function setName(name){
+// 	const name_user = name;
+// 	user_name.forEach( name => {
+// 		name.innerHTML = name_user;
+// 	});
+// 	return namee;
+// }
+	
 function nextStepEnterName(e){
 	const key = e.which || e.keyCode;
 	if(this.value == '' && key === 13){
@@ -15,6 +22,7 @@ function nextStepEnterName(e){
 	} 
 	else if(this.value !== '' && key === 13){
 		e.preventDefault();
+		//setName(this.value);
 		nextStep();
 	}
 }
@@ -44,6 +52,7 @@ function showApp(weight){
 
 function nextStepButtonName(e){
 	user_input_name.value ? nextStep() : false;
+	//setName(user_input_name.value);
 	e.preventDefault();
 }
 
@@ -56,9 +65,11 @@ function previousStep(e){
 	inputs.classList.remove("next-step");
 }
 
+
 user_input_submit.addEventListener("click", nextStepButtonName);
 user_input_name.addEventListener("keypress", nextStepEnterName);
 user_input_back.addEventListener("click", previousStep);
 
 user_input_submit2.addEventListener("click", nextStepButtonWeight);
 user_input_weight.addEventListener("keypress", nextStepEnterWeight);
+
